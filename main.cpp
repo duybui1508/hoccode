2539c8b76a1d38df2f29b9fe9ce7b52a64e8c114
DUYBUI <duyahihi1998@gmail.com>
2025-07-21 16:39:43 +0700

    fibo

---
+++ main.cpp
@@ -0,0 +1,19 @@
+ #include <bits/stdc++.h>
+ using namespace std;
+bool check (long long  n){
+ long long fibo[100];
+ fibo[0]=0;
+ fibo[1]=1;
+ for(int i=2; i<=92; i++){
+    fibo[i]=fibo[i-1] + fibo[i-2];
+ }
+ for(int i=2; i<=92; i++){
+    if(fibo[i]==n) return true;
+ }
+ return false;
+ }
+int main(){
+ int n;
+ cin >> n;
+ cout << check(n) << endl;
+}
